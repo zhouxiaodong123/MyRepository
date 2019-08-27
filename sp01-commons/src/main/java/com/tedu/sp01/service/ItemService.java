b8e84b516eec3c853263309650a0c93cda997059
tarena <tarena@176.201.112.7>
2019-08-27 14:54:57 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,11 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+	List<Item> getItems(String orderId);
+	//减少商品数量
+	void decreaseNumbers(List<Item> list);
+}
